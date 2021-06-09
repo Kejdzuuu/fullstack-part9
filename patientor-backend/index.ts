eb9b2e647554d6d8b3299897e1dd254ecefe7da8
Maciej Modrzejewski <kejdzuuu@gmail.com>
2021-06-09 12:18:11 +0200

    Ex. 9.9 fixed final

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-//import cors from 'cors';
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged');
     res.send('pong');
 });
