e342af032604a5ef7cd1844b32f0f4b535f7dcf0
Maciej Modrzejewski <kejdzuuu@gmail.com>
2021-06-16 12:45:02 +0200

    Ex. 9.12

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import patientService from '../services/patientService';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    res.send(patientService.getNonSensitiveEntries());
+  res.send(patientService.getNonSensitiveEntries());
+});
+
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatient = patientService.addPatient(
+    { name, dateOfBirth, ssn, gender, occupation }
+  );
+  res.json(newPatient);
 });
 
 export default router;
--- patientor-backend/src/services/patientService.ts
+++ patientor-backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patients from '../../data/patients';
-import { Patient, NonSensitivePatient } from '../types';
+import { Patient, NonSensitivePatient, NewPatient } from '../types';
+import {v1 as uuid} from 'uuid';
 
 const getEntries = (): Array<Patient> => {
   return patients;
@@ -9,4 +10,14 @@ const getNonSensitiveEntries = (): Array<NonSensitivePatient> => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({ id, name, dateOfBirth, gender, occupation }));
 };
 
-export default { getEntries, getNonSensitiveEntries };
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...patient
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default { getEntries, getNonSensitiveEntries, addPatient };
