1b47a93bd102a055a952417ca9695f37ae6ed4da
Maciej Modrzejewski <kejdzuuu@gmail.com>
2021-06-30 20:08:08 +0200

    Ex. 9.17

---
+++ patientor/src/PatientDetailsPage/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Patient } from "../types";
+import { useStateValue } from "../state";
+import { apiBaseUrl } from "../constants";
+import { Header, Icon, List } from "semantic-ui-react";
+import { SemanticICONS } from "semantic-ui-react/dist/commonjs/generic";
+
+
+const PatientDetailsPage = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+
+  const isPatientDataFetched = (): boolean => {
+    if (!patients || !patients[id] || !patients[id].ssn) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const getGenderIconName = (gender: string): SemanticICONS => {
+    switch (gender) {
+      case "male":
+        return "mars";
+      case "female":
+        return "venus";
+      default:
+        return "question";
+    }
+  };
+
+  React.useEffect(() => {
+    const fetchPatientDetails = async () => {
+      try {
+        const { data: patientDetails } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "UPDATE_PATIENT_INFO", payload: patientDetails });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+  
+    if (!isPatientDataFetched()) {
+      void fetchPatientDetails();
+    }
+  }, [dispatch]);
+
+  if (!isPatientDataFetched()) {
+    return (
+      <div>
+        Fetching data...
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Header as="h2">
+        {patients[id].name}
+        <Icon name={getGenderIconName(patients[id].gender)} />
+      </Header>
+      <List>
+        <List.Item>ssn: {patients[id].ssn}</List.Item>
+        <List.Item>occupation: {patients[id].occupation}</List.Item>
+      </List>
+    </div>
+  );
+};
+
+export default PatientDetailsPage;
--- patientor/src/state/reducer.ts
+++ patientor/src/state/reducer.ts
@@ -9,7 +9,11 @@ export type Action =
   | {
       type: "ADD_PATIENT";
       payload: Patient;
-    };
+    }
+  | {
+      type: "UPDATE_PATIENT_INFO";
+      payload: Patient;
+  };
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -25,6 +29,7 @@ export const reducer = (state: State, action: Action): State => {
         }
       };
     case "ADD_PATIENT":
+    case "UPDATE_PATIENT_INFO":
       return {
         ...state,
         patients: {
